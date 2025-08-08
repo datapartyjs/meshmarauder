ea658fe26810018b6789e3d76b333807ff21aa54
Dan Theisen <djt@hxx.in>
2025-08-07 21:45:42 -0700

    actually call set_clock

--- lora-scanner.sh
+++ lora-scanner.sh
@@ -117,6 +117,7 @@ preset_idx=0
 init_scanner() {
     stty -F ${DEVICE} raw
     stty -F ${DEVICE} -echo
+    set_clock
     change_preset ${PRESET[0]} # set initial preset
     preset_idx=1 # initialize preset_idx counter to begin on the second value
 }
